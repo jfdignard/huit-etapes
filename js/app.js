27da867772e8d41646ec6ecb78864dac492ae42e
heavymental33 <non_phixion418@hotmail.com>
2019-10-24 09:54:38 -0400

    Ajout d'un commentaire

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+// C'est le fichier principal de l'application pour le JS
         $(document).ready(function() {
             $("div").addClass("hide").fadeIn(2000).removeClass("hide");
         });
