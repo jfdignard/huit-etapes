718fadd30dc766c3438d54721ceb207c389e1494
jfdignard <jeff.dignard@outlook.com>
2019-11-26 14:08:05 -0500

    Modif de regles .gitignore/Ajout de source index.html/Ajout fonction anim app.js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@
         $(document).ready(function() {
             $("div").addClass("hide").fadeIn(2000).removeClass("hide");
         });
+// Ajout fait par jfdignard
+        $(document).ready(function() {
+            $("div").addClass("hide").fadeIn(2000).removeClass("hide");
+        });
